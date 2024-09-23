f0229eb55dc98aa03891fed6a918909aa85118db
iamRazzakk <rakibt23p@gmail.com>
2024-09-23 14:18:49 -0400

    update pastBooking component for show date

--- src/components/Dashboard/ForUser/PastBookings.tsx
+++ src/components/Dashboard/ForUser/PastBookings.tsx
@@ -1,9 +1,9 @@
-import { Table } from 'antd';
-import { useGetAllOrdersQuery } from '../../../redux/Api/orderApi/orderApi';
-import { useAppSelector } from '../../../redux/hooks';
-import { useCurrentUser } from '../../../redux/features/auth/authslice';
-import LoadingPage from '../../../pages/Loading/LoadingPage';
-import { Booking } from '../../../types/UpcommingTypes/Upcomming.Types';
+import { Table } from "antd";
+import { useGetAllOrdersQuery } from "../../../redux/Api/orderApi/orderApi";
+import { useAppSelector } from "../../../redux/hooks";
+import { useCurrentUser } from "../../../redux/features/auth/authslice";
+import LoadingPage from "../../../pages/Loading/LoadingPage";
+import { Booking } from "../../../types/UpcommingTypes/Upcomming.Types";
 
 const PastBookings: React.FC = () => {
   const { data: bookings, isLoading } = useGetAllOrdersQuery(undefined);
@@ -13,31 +13,33 @@ const PastBookings: React.FC = () => {
   const pastBookings = bookings?.data?.filter(
     (booking: Booking) => booking.user.email === currentUser?.email
   );
+  console.log(pastBookings);
 
   if (isLoading) return <LoadingPage />;
 
   const columns = [
     {
-      title: 'Service Name',
-      dataIndex: ['serviceDetails', 'serviceName'], 
-      key: 'serviceName',
+      title: "Service Name",
+      dataIndex: ["serviceDetails", "serviceName"],
+      key: "serviceName",
     },
     {
-      title: 'Date',
-      dataIndex: ['serviceDetails', 'startTime'], 
-      key: 'date',
+      title: "Date",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt: string) => new Date(createdAt).toLocaleDateString(),
     },
     {
-      title: 'Time',
+      title: "Time",
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       render: (_: any, record: Booking) =>
-        `${record.serviceDetails.startTime} - ${record.serviceDetails.endTime}`, 
-      key: 'time',
+        `${record.serviceDetails.startTime} - ${record.serviceDetails.endTime}`,
+      key: "time",
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
     },
   ];
 
